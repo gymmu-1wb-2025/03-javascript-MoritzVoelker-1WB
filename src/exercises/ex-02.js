37845667f6204bbf08ecd17515b069c647356b04
Moritz Völker <e228233@edu.sbl.ch>
2026-01-16 13:37:10 +0100

    Ich habe die Aufgabe drei gelöst, indem ich myage + 4 und Number am richtigen Ort hinzugefügt habe.

--- src/exercises/ex-02.js
+++ src/exercises/ex-02.js
@@ -1,4 +1,4 @@
-const myname = process. argv [2]
-const myage = process. argv [3]
+const myname = process. argv [2];
+const myage = Number (process. argv [3]); // Man muss Nuber schreiben, damit die Rechnung ausgeführt wird, sonst würde nur 164 stehen.
 
-console.log(`Hallo ${myname} dein alter ist ${myage}.`);
+console.log(`Hallo ${myname} dein alter ist ${myage + 4}.`);
